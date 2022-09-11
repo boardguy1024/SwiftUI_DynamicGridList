6b1521c86ff09a9aef305484b8e7c77f677ae725
boardguy1024 <boardguy1024@gmail.com>
2022-09-11 22:27:52 +0900

    Initial Commit

---
+++ SwiftUI_DynamicGridList/SwiftUI_DynamicGridListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DynamicGridListApp.swift
+//  SwiftUI_DynamicGridList
+//
+//  Created by park kyung seok on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DynamicGridListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
